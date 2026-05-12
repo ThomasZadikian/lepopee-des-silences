63589c3f4826248a8fb87328985cd740ec14e7d9
ThomasZadikian <Thoma.zadikian@gmail.com>
2026-05-12 15:32:00 +0200

    fix : restore enemy.speed

--- unity-client/Assets/Scripts/Combat/Combatant.cs
+++ unity-client/Assets/Scripts/Combat/Combatant.cs
@@ -46,7 +46,7 @@ public class Combatant
             maxHP = e.maxHP,
             currentMP = 0,
             maxMP = 0,
-            speed = e.speed * 4f,
+            speed = e.speed,
             atbCurrent = 0f,
             isPlayer = false,
             portraitColor = Color.red,
